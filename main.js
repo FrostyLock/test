cc176a98be192284cc9c528740b8b09824e010f9
FrostyLock <158774773+FrostyLock@users.noreply.github.com>
2024-02-06 20:03:10 +0200

    Update and rename main to main.js

---
+++ main.js
@@ -0,0 +1,59 @@
+// const xmlStr = `<list>
+//   <student>
+//     <name lang="en">
+//       <first>Ivan</first>
+//       <second>Ivanov</second>
+//     </name>
+//     <age>35</age>
+//     <prof>teacher</prof>
+//   </student>
+//   <student>
+//     <name lang="ru">
+//       <first>Петр</first>
+//       <second>Петров</second>
+//     </name>
+//     <age>58</age>
+//     <prof>driver</prof>
+//   </student>
+// </list>`;
+
+// const parser = new DOMParser();
+// const xmlDoc = parser.parseFromString(xmlStr, "application/xml");
+
+// const students = [];
+// const studentNodes = xmlDoc.getElementsByTagName("student");
+
+// for (let i = 0; i < studentNodes.length; i++) {
+//   const student = {};
+//   const name = studentNodes[i].getElementsByTagName("name")[0];
+//   const firstName = name.getElementsByTagName("first")[0].textContent;
+//   const lastName = name.getElementsByTagName("second")[0].textContent;
+//   const fullName = `${firstName} ${lastName}`;
+//   student.name = fullName;
+//   student.age = parseInt(studentNodes[i].getElementsByTagName("age")[0].textContent);
+//   student.prof = studentNodes[i].getElementsByTagName("prof")[0].textContent;
+//   student.lang = name.getAttribute("lang");
+//   students.push(student);
+// }
+
+// const result = { list: students };
+// console.log(result);
+
+const xmlStr = '<list><item><name>Petr</name><age>20</age><prof>mechanic</prof></item><item><name>Vova</name><age>60</age><prof>pilot</prof></item></list>';
+const parser = new DOMParser();
+const xmlDoc = parser.parseFromString(xmlStr, "application/xml");
+
+const list = [];
+const itemNodes = xmlDoc.getElementsByTagName("item");
+
+for (let i = 0; i < itemNodes.length; i++) {
+  const item = {};
+  item.name = itemNodes[i].getElementsByTagName("name")[0].textContent;
+  item.age = itemNodes[i].getElementsByTagName("age")[0].textContent;
+  item.prof = itemNodes[i].getElementsByTagName("prof")[0].textContent;
+  list.push(item);
+}
+
+const result = { list };
+console.log(result);
+
