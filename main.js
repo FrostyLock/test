1c3e765265f6fc8dd18b6f3f07dae6bfd7f2f931
FrostyLock <158774773+FrostyLock@users.noreply.github.com>
2024-02-10 16:27:35 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,43 +1,43 @@
-// const xmlStr = `<list>
-//   <student>
-//     <name lang="en">
-//       <first>Ivan</first>
-//       <second>Ivanov</second>
-//     </name>
-//     <age>20</age>
-//     <prof>teacher</prof>
-//   </student>
-//   <student>
-//     <name lang="ru">
-//       <first>Петр</first>
-//       <second>Петров</second>
-//     </name>
-//     <age>60</age>
-//     <prof>driver</prof>
-//   </student>
-// </list>`;
+const xmlStr = `<list>
+  <student>
+    <name lang="en">
+      <first>Ivan</first>
+      <second>Ivanov</second>
+    </name>
+    <age>20</age>
+    <prof>teacher</prof>
+  </student>
+  <student>
+    <name lang="ru">
+      <first>Петр</first>
+      <second>Петров</second>
+    </name>
+    <age>60</age>
+    <prof>driver</prof>
+  </student>
+</list>`;
 
-// const parser = new DOMParser();
-// const xmlDoc = parser.parseFromString(xmlStr, "application/xml");
+const parser = new DOMParser();
+const xmlDoc = parser.parseFromString(xmlStr, "application/xml");
 
-// const students = [];
-// const studentNodes = xmlDoc.getElementsByTagName("student");
+const students = [];
+const studentNodes = xmlDoc.getElementsByTagName("student");
 
-// for (let i = 0; i < studentNodes.length; i++) {
-//   const student = {};
-//   const name = studentNodes[i].getElementsByTagName("name")[0];
-//   const firstName = name.getElementsByTagName("first")[0].textContent;
-//   const lastName = name.getElementsByTagName("second")[0].textContent;
-//   const fullName = `${firstName} ${lastName}`;
-//   student.name = fullName;
-//   student.age = parseInt(studentNodes[i].getElementsByTagName("age")[0].textContent);
-//   student.prof = studentNodes[i].getElementsByTagName("prof")[0].textContent;
-//   student.lang = name.getAttribute("lang");
-//   students.push(student);
-// }
+for (let i = 0; i < studentNodes.length; i++) {
+  const student = {};
+  const name = studentNodes[i].getElementsByTagName("name")[0];
+  const firstName = name.getElementsByTagName("first")[0].textContent;
+  const lastName = name.getElementsByTagName("second")[0].textContent;
+  const fullName = `${firstName} ${lastName}`;
+  student.name = fullName;
+  student.age = parseInt(studentNodes[i].getElementsByTagName("age")[0].textContent);
+  student.prof = studentNodes[i].getElementsByTagName("prof")[0].textContent;
+  student.lang = name.getAttribute("lang");
+  students.push(student);
+}
 
-// const result = { list: students };
-// console.log(result);
+const result = { list: students };
+console.log(result);
 
 const xmlStr = '<list><item><name>Petr</name><age>20</age><prof>mechanic</prof></item><item><name>Vova</name><age>60</age><prof>pilot</prof></item></list>';
 const parser = new DOMParser();
